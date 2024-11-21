7b42a1e197c7eb29a5237fc4b8aeeae21671eb8a
ThisIsLuckyluck <denislucas1a@gmail.com>
2024-11-21 11:47:06 +0100

    Update app.js by adding previous dependencies and adding a route

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
+require("dotenv").config();
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var tweetsRouter = require("./routes/tweets");
 
 var app = express();
 
-var cors = require("cors");
+const cors = require("cors");
 app.use(cors());
 
 app.use(logger("dev"));
@@ -18,5 +21,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/tweets", tweetsRouter);
 
 module.exports = app;
