52b5e0b75f7de0e057984baa4f198a7066323910
ThisIsLuckyluck <denislucas1a@gmail.com>
2024-11-21 11:49:43 +0100

    updating users routes with Signup, Signin and get request. Initialization of the tweets routes with CRUD operations

---
+++ routes/tweets.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var router = express.Router();
+
+router.post("/create", (req, res) => {});
+
+router.get("/read", (req, res) => {});
+
+router.put("/update", (req, res) => {});
+
+router.delete("/delete", (req, res) => {});
+
+module.exports = router;
